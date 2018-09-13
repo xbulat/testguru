ac442345544f18e972eb8a7ef474c199e8380da3
Alexandr Bulatov <cruc@enicad.com>
2018-09-13 23:35:04 +0300

    Fill information
    - Add StaticPage - About Project
    - Add StaticPage - About Author
    - Update README.md
    - Set method version project
    - Set locales
    - Set timezone

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module TestGuru
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.default_locale = :ru
+    config.time_zone = 'Moscow'
+    config.active_record.default_timezone = :local
   end
 end
---
+++ config/initializers/version.rb
@@ -0,0 +1,5 @@
+module TestGuru
+  def self.version
+    '0.1.1'
+  end
+end
