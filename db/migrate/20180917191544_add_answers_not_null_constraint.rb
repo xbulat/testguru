c0706dca9cc1cf492f0b908d462ab17be43a3587
Alexandr Bulatov <cruc@enicad.com>
2018-09-17 22:18:33 +0300

    Add base models and migrations
    * Models
    ** Categories
    ** Tests
    ** Questions
    ** Answers
    ** Users
    
    * Migrations
    ** Default values for tests and answers
    ** NotNULL constraints for data consistency

---
+++ db/migrate/20180917191544_add_answers_not_null_constraint.rb
@@ -0,0 +1,6 @@
+class AddAnswersNotNullConstraint < ActiveRecord::Migration[5.2]
+  def change
+    change_column_null(:answers, :body, false)
+    change_column_null(:answers, :question_id, false)  
+  end
+end
