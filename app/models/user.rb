bfdb683fb6031a0e95460b58aede50aae51d4d12
Alexandr Bulatov <cruc@enicad.com>
2018-10-30 21:04:34 +0200

    Add model sti Admin Model User use devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   has_many :user_passed_tests
   has_many :tests, through: :user_passed_tests
   has_many :author_tests, class_name: 'Test'
 
-  has_secure_password
 
   validates :first_name, :last_name, presence: true
   validates :email, presence: true,
