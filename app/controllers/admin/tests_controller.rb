085567e2140f8231cce54cf45a9215ee19aa9747
Alexandr Bulatov <cruc@enicad.com>
2018-10-30 21:03:09 +0200

    Move controllers under admin namespace Add authentication with help devise

---
+++ app/controllers/admin/tests_controller.rb
@@ -0,0 +1,59 @@
+class Admin::TestsController < Admin::BaseController
+  before_action :find_test, only: %i[show destroy edit update start]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
+
+  def index
+    @tests = Test.all
+  end
+
+  def show; end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = current_user.author_tests.new(resource_params)
+
+    if @test.save
+      redirect_to admin_test_path(@test)
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @test.update(resource_params)
+      redirect_to admin_test_path(@test)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @test.destroy
+    redirect_to admin_tests_path
+  end
+
+  def start
+    current_user.tests.push(@test)
+    redirect_to current_user.user_passed_test(@test)
+  end
+
+  private
+
+  def resource_params
+    params.require(:test).permit(:title, :level, :category_id)
+  end
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
+
+  def rescue_with_test_not_found
+    render inline: 'Test not found [404]', status: 404
+  end
+end
--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,41 +1,10 @@
 class TestsController < ApplicationController
-  before_action :find_test, only: %i[show destroy edit update start]
+  before_action :find_test, only: :start
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
   def index
     @tests = Test.all
-  end
-
-  def show; end
-
-  def new
-    @test = Test.new
-  end
-
-  def create
-    @test = current_user.author_tests.new(resource_params)
-
-    if @test.save
-      redirect_to @test
-    else
-      render :new
-    end
-  end
-
-  def edit; end
-
-  def update
-    if @test.update(resource_params)
-      redirect_to @test
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @test.destroy
-    redirect_to tests_path
   end
 
   def start
@@ -44,10 +13,6 @@ class TestsController < ApplicationController
   end
 
   private
-
-  def resource_params
-    params.require(:test).permit(:title, :level, :category_id)
-  end
 
   def find_test
     @test = Test.find(params[:id])
