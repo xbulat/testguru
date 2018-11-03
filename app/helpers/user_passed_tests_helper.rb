80f5e44116a833ad3ad68c3dc6f45ef552ff0556
Alexandr Bulatov <cruc@enicad.com>
2018-11-03 17:19:39 +0200

    Replace message to i18n helpers in helpres

--- app/helpers/user_passed_tests_helper.rb
+++ app/helpers/user_passed_tests_helper.rb
@@ -1,6 +1,6 @@
 module UserPassedTestsHelper
   def result_message(user_passed_test)
-    user_passed_test.success? ? 'Test was passed!' : 'Test was failed!'
+    user_passed_test.success? ? t('user_passed_tests.result.passed') : t('user_passed_tests.result.failed')
   end
 
   def result_css_style(user_passed_test)
